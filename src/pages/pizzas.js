3c645cfd30b9d0923829998709d10b257361b6e9
Chris Young <chrisyoung0101@gmail.com>
2020-10-02 20:06:46 -0500

    finish Module #5

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import { graphql } from 'gatsby';
+import PizzasList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey! I'm the Pizzas page</p>
+      <PizzasList pizzas={pizzas} />
     </>
   );
 }
+
+// name it what you like but there is most likely a convention
+export const query = graphql`
+  #name the query or not
+  query PizzaQuery {
+    #rename to pizzas
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              #GraphQL fragment : collection of fields that we don't have to retype separately
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
